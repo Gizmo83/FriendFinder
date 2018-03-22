db8cf06408e845b8ff1ac734cc30a8ddb329648f
Gizmo83 <p_hoang83@yahoo.com>
2018-03-21 19:21:24 -0700

    Fixed apiRoutes.js and updated survey.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,15 @@
-var friends = require("./data/friends");
+var path = require("path");
+var friends = require("../data/friends");
 
-app.get("/api/friends", function(req, res) {
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
 
-    return res.json(characters);
-  });
+        return res.json(characters);
+    });
 
-app.post("/api/friends", function(req, res) {
-    var newFriend = req.body;
-    friends.push(newFriend);
-    res.json(newFriend);
-});
+    app.post("/api/friends", function(req, res) {
+        var newFriend = req.body;
+        friends.push(newFriend);
+        res.json(newFriend);
+    });
+};
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
-// require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
